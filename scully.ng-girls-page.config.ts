616d7390c4d3950c4d439f99a58f7219d9b01833
Dominika Zając <dominikaz@qualtrics.com>
2020-05-26 18:27:36 +0200

    fix config file extension

---
+++ scully.ng-girls-page.config.ts
@@ -0,0 +1,22 @@
+require('./extraPlugin/workshop-add-flex');
+
+exports.config = {
+  projectRoot: "./src",
+  projectName: "ng-girls-page",
+  outDir: './dist/static',
+  routes: {
+    '/blog/:slug': {
+      type: 'contentFolder',
+      slug: {
+        folder: "./blog"
+      }
+    },
+    '/workshops/:workshopId': {
+      type: 'contentFolder',
+      postRenderers: ['addFlex'],
+      workshopId: {
+        folder: "./workshops"
+      }
+    }
+  }
+};
