f218194b3a36ea44b3acfb71139894f3d31d9d29
Ankita Singhal <AnkitaSinghal@users.noreply.github.com>
2020-05-28 08:43:48 +0200

    :hammer: using scully server to generate static routes from json
    We can crawl the json file from scully server and generate static routes

--- scully.ng-girls-page.config.ts
+++ scully.ng-girls-page.config.ts
@@ -1,46 +1,26 @@
 require('./extraPlugin/workshop-add-flex');
 
-const { registerPlugin } = require('@scullyio/scully');
-
-import { readFileSync } from 'fs';
-
- async function dynamicRoutePlugin(angularRoute, conf = {}) {
-  const parts = angularRoute.split('/');
-  const param = parts.filter(p => p.startsWith(':')).map(id => id.slice(1))[0];
-  const paramConfig = conf[param];
-  return await checkSourceIsDirectoryAndRun(paramConfig['path'],paramConfig['property'], angularRoute.split(':')[0]);
-}
-
-async function checkSourceIsDirectoryAndRun(path, prop, newroute) {
-  const files =  readFileSync(path, 'utf-8');
-  const jsonFile = JSON.parse(files);
-  let handledRoutes = [];
-  for (const workshop of jsonFile) {
-    handledRoutes.push(
-      { route: newroute + workshop[prop] })
-  }
-  return handledRoutes;
-}
-
-registerPlugin('router', 'dynamicRoute', dynamicRoutePlugin);
-
 exports.config = {
-  projectRoot: "./src",
-  projectName: "ng-girls-page",
+  projectRoot: './src',
+  projectName: 'ng-girls-page',
   outDir: './dist/static',
   routes: {
     '/blog/:slug': {
       type: 'contentFolder',
       slug: {
-        folder: "./blog"
+        folder: './blog'
       }
     },
     '/workshops/:workshopId': {
-      type: 'dynamicRoute',
+      type: 'json',
       workshopId: {
-        property: 'route',
-        path:'src/assets/data/workshops.json'
-      }
+        url: 'http://localhost:1668/assets/data/workshops.json',
+        id: 'title',
+        resultsHandler: function (raw) {
+          return raw.map(a => a.title);
+        },
+      },
+      postRenderers: ['addFlex'],
     },
     // '/workshops/:workshopId': {
     //   type: 'contentFolder',
