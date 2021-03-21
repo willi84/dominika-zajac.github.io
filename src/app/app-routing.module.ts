2b002fbe5bb99433a45ff57c085b7489ed0b9903
Robert Willemelis <robert.dev@gmx.de>
2021-03-21 21:39:10 +0100

    fix: deactivate path match route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'workshops', loadChildren: () => import('./workshops/workshops.module').then(m => m.WorkshopsModule) },
   // test
-  { path: 'krakow',   redirectTo: '/workshops/krakow', pathMatch: 'full' },
+  // { path: 'krakow',   redirectTo: '/workshops/krakow', pathMatch: 'full' },
   { path: 'faq', component: FaqComponent },
   { path: 'coc', component: CocComponent },
   { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) },
