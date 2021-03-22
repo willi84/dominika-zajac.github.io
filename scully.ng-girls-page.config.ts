e72e989b9c833ee8d8e44651517bd403237c07ce
Robert Willemelis <robert.dev@gmx.de>
2021-03-22 22:53:58 +0100

    feat: add disable angular plugin

--- scully.ng-girls-page.config.ts
+++ scully.ng-girls-page.config.ts
@@ -1,9 +1,14 @@
 require('./extraPlugin/workshop-add-flex');
+import { ScullyConfig } from '@scullyio/scully';
+const {DisableAngular} = require('scully-plugin-disable-angular');
+
+const postRenderers = [DisableAngular];
 
 exports.config = {
   projectRoot: "./src",
   projectName: "ng-girls-page",
   outDir: './dist/static',
+  defaultPostRenderers: postRenderers,
   routes: {
     '/blog/:slug': {
       type: 'contentFolder',
